f1db5cce90c6f271a361b256e6e67658ed9384c5
Alfred Jayaprakash <alfredisfun@gmail.com>
2020-06-29 21:58:11 +0800

    Updated server.js

--- src/server/server.js
+++ src/server/server.js
@@ -8,19 +8,7 @@ const {handleNewConnection} = require ('./utils/gameserver');
 const port = process.env.PORT || 3001;
 
 //Setup express
-const app = express ();
-
-//Setup static files directory
-app.use (express.static (path.join (__dirname, '../../build')));
-
-//By default load the index.html that will load the React SPA
-app.get ('/', function (req, res) {
-  res.sendFile (path.join (__dirname, '../../build', 'index.html'));
-});
-
-//Setup the rooms router to serve requests coming to /rooms
-app.use (express.json ());
-app.use ('/rooms', rooms); //Use the rooms router
+const app = require ('./app');
 
 //Create Socket.io server
 const server = http.createServer (app);
